7fd89a8cd8f563447f61c916a2567fc52a45569e
chandu141186 <chandu141186@gmail.com>
2024-07-09 11:08:04 +0530

    mongodb installtion process

--- catlouge.sh
+++ catlouge.sh
@@ -27,22 +27,41 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disabling Nodjs "
+
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+VALIDATE $? "enabling Nodjs18 "
+
+dnf install nodejs -y  &>> $LOGFILE
+VALIDATE $? "Installing DNodejs "
+
 useradd roboshop
 mkdir /app
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip  &>> $LOGFILE
+
+VALIDATE $? "Copying catloguge application from url "
+
 cd /app 
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip  &>> $LOGFILE
+
+VALIDATE $? "Unzipping Catalouge application"
+
 cd /app
-npm install 
+npm install &>> $LOGFILE
+VALIDATE $? "Installing Applcation "
 
 
 systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+VALIDATE $? "Reloding app "
 
-dnf install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+systemctl enable catalogue
+VALIDATE $? "Enabling Catalouge "
+
+systemctl start catalogue
+VALIDATE $? "Starting Catalouge "
+
+
+dnf install mongodb-org-shell -y  &>> $LOGFILE
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js &>> $LOGFILE
