67f82ec74e665ac0c48b00d138f3a964293fdea3
chandu141186 <chandu141186@gmail.com>
2024-07-10 18:01:15 +0530

    variable commit

--- robhoshopshell.sh
+++ robhoshopshell.sh
@@ -6,11 +6,12 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catlouge" "user" "cart" "shippi
 
 for i in "{INSTANCES(@)}"
 do
+  echo "instance is:$i"
  if [ $i == "mongdb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
  then 
      INSTANCE_TYPE="t3.small"
  else 
      INSTANCE_TYPE="t2.micro"
  fi
-aws ec2 run-instances --image-id  ami-0b4f379183e5706b9 --count 1 --instance-type t2.micro --security-group-ids sg-04e8a790706df00aa
+aws ec2 run-instances --image-id  ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-04e8a790706df00aa
  done
