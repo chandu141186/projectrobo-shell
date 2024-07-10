6c29afced12fd754b212e8d25b7d8bbb0b356b42
chandu141186 <chandu141186@gmail.com>
2024-07-10 18:38:02 +0530

    variable commit

--- robhoshopshell.sh
+++ robhoshopshell.sh
@@ -7,11 +7,11 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catlouge" "user" "cart" "shippi
 for i in "${INSTANCES[@]}"
 do
   echo "instance is:$i"
- if [ $i == "mongdb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
- then 
+  if [ $i == "mongdb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+  then 
      INSTANCE_TYPE="t3.small"
- else 
+  else 
      INSTANCE_TYPE="t2.micro"
- fi
-aws ec2 run-instances --image-id  ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-04e8a790706df00aa --tag-specifications "ResourceType=instance,Tags=[{Key=Name ,Value=$i}]"
+  fi
+    IP_ADDRESS=$(aws ec2 run-instances --image-id  ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-04e8a790706df00aa --tag-specifications "ResourceType=instance,Tags=[{Key=Name ,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
  done
