f79254c0715ef9720838194980a30c3e38c4f821
chandu141186 <chandu141186@gmail.com>
2024-07-11 10:38:03 +0530

    variable commit

--- robhoshopshell.sh
+++ robhoshopshell.sh
@@ -3,6 +3,8 @@
 AMI=ami-0b4f379183e5706b9 
 SG_ID=sg-04e8a790706df00aa
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catlouge" "user" "cart" "shipping" "payment" "dispatch" "web" )
+ZONE_ID=Z06286322TWN251SZBYW9
+DOMAIN_NAME=chandulearn.online
 
 for i in "${INSTANCES[@]}"
 do
@@ -14,5 +16,23 @@ do
   fi
     IP_ADDRESS=$(aws ec2 run-instances --image-id  ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-04e8a790706df00aa --tag-specifications "ResourceType=instance,Tags=[{Key= Name,Value= $i}]" --query 'Instances[0].PrivateIpAddress' --output text)
       echo "$i: $IP_ADDRESS"
+     
+   aws route53 change-resource-record-sets \
+   --hosted-zone-id $ZONE_ID \
+   --change-batch '
+  {
+    "Comment": "Creating a record set for cognito endpoint"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$i'.'$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP_ADDRESS'"
+        }]
+      }
+    }]
+  }
 
  done
