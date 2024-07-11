4a4d5f6435e3481d79cbe3964bb9b0eaa40c68c2
chandu141186 <chandu141186@gmail.com>
2024-07-11 10:00:25 +0530

    variable commit

--- robhoshopshell.sh
+++ robhoshopshell.sh
@@ -12,9 +12,7 @@ do
   else 
      INSTANCE_TYPE="t2.micro"
   fi
-    IP_ADDRESS=$(aws ec2 run-instances --image-id  ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE 
-                 --security-group-ids sg-04e8a790706df00aa --tag-specifications "ResourceType=instance,Tags=[{Key=Name ,Value=$i}]" 
-                 --query 'Instances[0].PrivateIpAddress' --output text)
-      echo "$i : $IP_ADDRESS"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id  ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-04e8a790706df00aa --tag-specifications "ResourceType=instance,Tags=[{Key= Name,Value= $i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+      echo "$i: $IP_ADDRESS"
 
  done
