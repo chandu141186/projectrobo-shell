f15a06204a5aba5146a6f951b0347c50d7edafab
chandu141186 <chandu141186@gmail.com>
2024-07-14 15:27:45 +0530

    variable commit

--- robhoshopshell.sh
+++ robhoshopshell.sh
@@ -17,13 +17,13 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id  ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-04e8a790706df00aa --tag-specifications "ResourceType=instance,Tags=[{Key= Name,Value= $i}]" --query 'Instances[0].PrivateIpAddress' --output text)
       echo "$i: $IP_ADDRESS"
 
-  if [ $i == "web" ]
-  then
-    IP_ADDRESS="Public IPv4 address"
-  else
-    IP_ADDRESS="Private IPv4 addresses"
+  #if [ $i == "web" ]
+  #then
+   # IP_ADDRESS="Public IPv4 address"
+  #else
+   # IP_ADDRESS="Private IPv4 addresses"
 
-  fi  
+  #fi  
      
    aws route53 change-resource-record-sets \
    --hosted-zone-id $ZONE_ID \
